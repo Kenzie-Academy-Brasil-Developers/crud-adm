dd38f32394c408b2a476b44c96e6b7c741f7aa2a
Vitor Galdino <httpvittor@gmail.com>
2023-02-13 19:57:41 -0300

    feat: included new route handle with express

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express, { Application } from 'express';
+import { userRoutes } from './routers/users';
 
 export const app: Application = express();
 app.use(express.json());
+
+app.use('/users', userRoutes);
