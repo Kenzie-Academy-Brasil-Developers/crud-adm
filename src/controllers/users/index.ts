518d42b7841667fd8e433e3e25b1793e4facdb18
Vitor Galdino <httpvittor@gmail.com>
2023-02-13 20:23:10 -0300

    feat: created new controller "createUsersController"

---
+++ src/controllers/users/index.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { IUserRequest } from '../../interfaces/users';
+import { createUsersService } from '../../services/users';
+
+export const createUsersController = async (req: Request, res: Response): Promise<Response> => {
+    const body: IUserRequest = req.body;
+
+    const userData = await createUsersService(body);
+
+    return res.status(201).json(userData);
+};
