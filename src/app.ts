ab553ea1daa80e109ac66cf5b225c11b9a0f8483
Vitor Galdino <httpvittor@gmail.com>
2023-02-14 16:14:12 -0300

    feat: included handleErrors in express

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Application } from 'express';
+import { handleErrors } from './errors';
 import { userRoutes } from './routers/users';
 
 export const app: Application = express();
 app.use(express.json());
 
 app.use('/users', userRoutes);
+
+app.use(handleErrors);
