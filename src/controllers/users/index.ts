78826930f5d6daa33f54a9d21967ded238b81c85
Vitor Galdino <httpvittor@gmail.com>
2023-02-14 15:58:39 -0300

    feat: renamed services/users file to "createUsers.ts"

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { IUserRequest } from '../../interfaces/users';
-import { createUsersService } from '../../services/users';
+import { createUsersService } from '../../services/users/createUsers';
 
 export const createUsersController = async (req: Request, res: Response): Promise<Response> => {
     const body: IUserRequest = req.body;
