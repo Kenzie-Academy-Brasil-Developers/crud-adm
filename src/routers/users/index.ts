ca727bc97ab5883955ffcb5c51a7a9ea0ba54c41
Vitor Galdino <httpvittor@gmail.com>
2023-02-14 17:16:26 -0300

    feat: included "checkUserEmail" middleware on POST route

--- src/routers/users/index.ts
+++ src/routers/users/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { createUsersController } from '../../controllers/users';
+import { checkUserEmail } from '../../middlewares/users';
 
 export const userRoutes: Router = Router();
 
-userRoutes.post('', createUsersController);
+userRoutes.post('', checkUserEmail, createUsersController);
