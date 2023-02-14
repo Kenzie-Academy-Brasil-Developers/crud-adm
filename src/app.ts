372c639af200bc8cc23fab8ce21032218bea908a
Vitor Galdino <httpvittor@gmail.com>
2023-02-14 16:50:49 -0300

    fix: import on top "express-async-errors"

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
+import 'express-async-errors';
 import express, { Application } from 'express';
 import { handleErrors } from './errors';
 import { userRoutes } from './routers/users';
-import 'express-async-errors'
 
 export const app: Application = express();
 app.use(express.json());
