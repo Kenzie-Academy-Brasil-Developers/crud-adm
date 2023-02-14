ce44140232633b06aeb0ef67a6ffde7545191f79
Vitor Galdino <httpvittor@gmail.com>
2023-02-14 16:13:05 -0300

    feat: created handleErrors middleware to handle with AppError class

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,5 @@
+import { NextFunction, Request, Response } from 'express';
+
 export class AppError extends Error {
     message: string;
     statusCode: number;
@@ -8,3 +10,11 @@ export class AppError extends Error {
         this.statusCode = statusCode;
     }
 }
+
+export const handleErrors = (error: Error, res: Response, req: Request, next: NextFunction) => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ message: error.message });
+    }
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+};
