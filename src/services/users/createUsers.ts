1440ea862695b722fe020a2a3e642043cbefcfa6
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 16:32:50 -0300

    feat: included hash to password encrypted

--- src/services/users/createUsers.ts
+++ src/services/users/createUsers.ts
@@ -1,21 +1,26 @@
+import { hash } from 'bcryptjs';
 import format from 'pg-format';
-
 import { client } from '../../database/config';
+
 import { tUserRequest, tUserResult, tUserWithoutPwd } from '../../interfaces/users';
+import { UserWithoutPwdSchema } from '../../schemas/users';
 
 export const createUsersService = async (payload: tUserRequest): Promise<tUserWithoutPwd> => {
+
+    const hashedPwd = await hash(payload.password, 10);
+    payload.password = hashedPwd;
 
     const queryString: string = format(`
         INSERT INTO
             users(%I)
         VALUES(%L)
-        RETURNING id, "name", email, "admin", active;
+        RETURNING *;
     `,
         Object.keys(payload),
         Object.values(payload)
     );
 
-    const QueryResult: tUserResult = await client.query(queryString);
+    const queryResult: tUserResult = await client.query(queryString);
 
-    return QueryResult.rows[0];
+    return UserWithoutPwdSchema.parse(queryResult.rows[0]);
 };
