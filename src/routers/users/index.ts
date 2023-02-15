f447e29a6d47103b124de2a64ed00e0796a5eb05
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 19:47:26 -0300

    feat: included validateBody middleware on loginRoute

--- src/routers/users/index.ts
+++ src/routers/users/index.ts
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import { createUsersController, loginController } from '../../controllers/users';
 import { checkUserEmail } from '../../middlewares/users';
 import { validateBody } from '../../middlewares/validateBody';
-import { userSchema } from '../../schemas/users';
+import { loginSchema, userSchema } from '../../schemas/users';
 
 export const userRoutes: Router = Router();
 export const loginRoute: Router = Router();
 
 userRoutes.post('', validateBody(userSchema), checkUserEmail, createUsersController);
 
-loginRoute.post('', loginController);
+loginRoute.post('', validateBody(loginSchema), loginController);
