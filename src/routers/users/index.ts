29cb56aed25340a7c24fbfeaa2463bef4bbf3199
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 17:25:25 -0300

    feat: created loginRoute with POST verb

--- src/routers/users/index.ts
+++ src/routers/users/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { createUsersController } from '../../controllers/users';
+import { createUsersController, loginController } from '../../controllers/users';
 import { checkUserEmail } from '../../middlewares/users';
 import { validateBody } from '../../middlewares/validateBody';
 import { userSchema } from '../../schemas/users';
 
 export const userRoutes: Router = Router();
+export const loginRoute: Router = Router();
 
 userRoutes.post('', validateBody(userSchema), checkUserEmail, createUsersController);
+
+loginRoute.post('', loginController);
