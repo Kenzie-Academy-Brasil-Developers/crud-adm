cc57cfe1c7f99c2bd42849f2b8fa40e0888954be
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 15:21:32 -0300

    feat: included validateBody middleware on POST route

--- src/routers/users/index.ts
+++ src/routers/users/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { createUsersController } from '../../controllers/users';
 import { checkUserEmail } from '../../middlewares/users';
+import { validateBody } from '../../middlewares/users/validateBody';
+import { userSchema } from '../../schemas/users';
 
 export const userRoutes: Router = Router();
 
-userRoutes.post('', checkUserEmail, createUsersController);
+userRoutes.post('', validateBody(userSchema), checkUserEmail, createUsersController);
