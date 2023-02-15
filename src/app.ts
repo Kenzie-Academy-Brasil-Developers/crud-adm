f866593c6b1c67326d99845f5107e0279afaa0ce
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 17:26:07 -0300

    feat: created endpoint "/login" to loginRoute

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express, { Application } from 'express';
 import 'express-async-errors';
 import { handleErrors } from './middlewares/errorHandler';
-import { userRoutes } from './routers/users';
+import { loginRoute, userRoutes } from './routers/users';
 
 export const app: Application = express();
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/login', loginRoute);
 
 app.use(handleErrors);
