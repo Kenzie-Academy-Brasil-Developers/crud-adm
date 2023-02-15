22de64747c1fa3a526508867796f94d052b0553b
Vitor Galdino <httpvittor@gmail.com>
2023-02-14 21:34:54 -0300

    feat: change queryResult type on checkUserEmail middleware

--- src/middlewares/users/index.ts
+++ src/middlewares/users/index.ts
@@ -2,21 +2,21 @@ import { NextFunction, Request, Response } from 'express';
 import format from 'pg-format';
 import { client } from '../../database/config';
 import { AppError } from '../../errors';
-import { UserResult } from '../../interfaces/users';
+import { UserOnlyWithEmailResult } from '../../interfaces/users';
 
 export const checkUserEmail = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
     if (req.body.email) {
 
         const queryString: string = format(`
-            SELECT * FROM users WHERE email = %L;
+            SELECT users.email FROM users WHERE email = %L;
         `,
             req.body.email
         );
 
-        const queryResult: UserResult = await client.query(queryString);
+        const queryResult: UserOnlyWithEmailResult = await client.query(queryString);
 
         if (queryResult.rowCount) {
-            throw new AppError('E-mail already registered', 409)
+            throw new AppError('E-mail already registered', 409);
         }
 
     }
