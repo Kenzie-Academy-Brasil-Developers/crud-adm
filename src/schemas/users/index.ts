590c6ef4c0140b597a9ff36182efdd242a606a8d
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 14:15:17 -0300

    feat: created userSchema with zod

---
+++ src/schemas/users/index.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const userSchema = z.object({
+    name: z.string().min(3).max(20),
+    email: z.string().email().max(100),
+    password: z.string().min(3).max(120),
+    admin: z.boolean(),
+    active: z.boolean()
+});
