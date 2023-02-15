00dbe12433fd7e4a3598e2eb25f8ef28c17ee4f4
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 19:04:19 -0300

    feat: created "tLogin" type

--- src/interfaces/users/index.ts
+++ src/interfaces/users/index.ts
@@ -1,10 +1,11 @@
 import { QueryResult } from 'pg';
 import { z } from 'zod';
-import { UserOnlyWithEmailSchema, userRequestSchema, userSchema, UserWithoutPwdSchema } from '../../schemas/users';
+import { loginSchema, UserOnlyWithEmailSchema, userRequestSchema, userSchema, UserWithoutPwdSchema } from '../../schemas/users';
 
 export type tUserRequest = z.infer<typeof userSchema>;
 export type tUser = z.infer<typeof userRequestSchema>;
 export type tUserWithoutPwd = z.infer<typeof UserWithoutPwdSchema>;
+export type tLogin = z.infer<typeof loginSchema>;
 
 export type tUserResult = QueryResult<tUserWithoutPwd>;
 export type tUserOnlyWithEmailResult = QueryResult<z.infer<typeof UserOnlyWithEmailSchema>>;
