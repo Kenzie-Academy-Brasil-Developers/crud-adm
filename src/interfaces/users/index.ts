b4bbcfebf6f6030f59241883fce14e1426b5779b
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 19:46:53 -0300

    feat: created "IToken" interface

--- src/interfaces/users/index.ts
+++ src/interfaces/users/index.ts
@@ -2,6 +2,10 @@ import { QueryResult } from 'pg';
 import { z } from 'zod';
 import { loginSchema, UserOnlyWithEmailSchema, userRequestSchema, userSchema, UserWithoutPwdSchema } from '../../schemas/users';
 
+export interface IToken {
+    token: string;
+}
+
 export type tUserRequest = z.infer<typeof userSchema>;
 export type tUser = z.infer<typeof userRequestSchema>;
 export type tUserWithoutPwd = z.infer<typeof UserWithoutPwdSchema>;
