ac9493c98dd2a041493445ca18f2ac46cde5bda9
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 19:09:16 -0300

    feat: created "loginController"

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
-import { tUserRequest } from '../../interfaces/users';
+import { tLogin, tUserRequest } from '../../interfaces/users';
 import { createUsersService } from '../../services/users/createUsers';
+import { loginUsersService } from '../../services/users/loginUsers';
 
 export const createUsersController = async (req: Request, res: Response): Promise<Response> => {
     const body: tUserRequest = req.body;
@@ -8,4 +9,12 @@ export const createUsersController = async (req: Request, res: Response): Promis
     const userData = await createUsersService(body);
 
     return res.status(201).json(userData);
+};
+
+export const loginController = async (req: Request, res: Response): Promise<Response> => {
+    const body: tLogin = req.body;
+
+    const token = await loginUsersService(body);
+
+    return res.json(token);
 };
