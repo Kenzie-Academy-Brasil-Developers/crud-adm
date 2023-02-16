9a97f277b04ee5033dadaff969f34f664df4ba88
Vitor Galdino <httpvittor@gmail.com>
2023-02-16 01:11:10 -0300

    feat: created "checkUserId" middleware

--- src/middlewares/users/index.ts
+++ src/middlewares/users/index.ts
@@ -83,3 +83,19 @@ export const checkPermission = (req: Request, res: Response, next: NextFunction)
 
     return next();
 };
+
+export const checkUserId = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    const queryString: string = format(`
+        SELECT * FROM users WHERE id = %s;
+    `,
+        req.params.id
+    );
+
+    const queryResult: tUserResult = await client.query(queryString);
+
+    if (!queryResult.rowCount) {
+        throw new AppError('User not found', 404);
+    }
+
+    return next();
+};
