3a232e090c45e9bc0e1eb2f3ba3552e46b5dd7aa
Vitor Galdino <httpvittor@gmail.com>
2023-02-16 00:39:22 -0300

    feat: created "tUpdateRequest"type

--- src/interfaces/users/index.ts
+++ src/interfaces/users/index.ts
@@ -1,6 +1,6 @@
 import { QueryResult } from 'pg';
 import { z } from 'zod';
-import { loginSchema, UserOnlyWithEmailSchema, userRequestSchema, userSchema, UserWithoutPwdSchema } from '../../schemas/users';
+import { loginSchema, UserOnlyWithEmailSchema, userRequestSchema, userSchema, userUpdateSchema, UserWithoutPwdSchema } from '../../schemas/users';
 
 export interface IToken {
     token: string;
@@ -10,6 +10,7 @@ export type tUserRequest = z.infer<typeof userSchema>;
 export type tUser = z.infer<typeof userRequestSchema>;
 export type tUserWithoutPwd = z.infer<typeof UserWithoutPwdSchema>;
 export type tLogin = z.infer<typeof loginSchema>;
+export type tUpdateRequest = z.infer<typeof userUpdateSchema>;
 
 export type tUserResult = QueryResult<tUser>;
 export type tUserOnlyWithEmailResult = QueryResult<z.infer<typeof UserOnlyWithEmailSchema>>;
