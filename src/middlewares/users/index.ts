a98bcb520e073201d58ff351cce9b14e8801511f
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 23:38:39 -0300

    feat: included new request property

--- src/middlewares/users/index.ts
+++ src/middlewares/users/index.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import format from 'pg-format';
+
 import { client } from '../../database/config';
 import { AppError } from '../../errors';
 import { tUserOnlyWithEmailResult, tUserResult } from '../../interfaces/users';
@@ -33,13 +34,16 @@ export const verifyToken = async (req: Request, res: Response, next: NextFunctio
 
     const token: string = authToken.split(' ')[1];
 
-    verify(
+    const userEmail: string | void = verify(
         token,
         String(process.env.SECRET_KEY),
         (error: any, decoded: any) => {
             if (error) throw new AppError(error.message, 401);
+            return decoded.email;
         }
     );
+
+    req.userEmail = userEmail;
 
     return next();
 };
