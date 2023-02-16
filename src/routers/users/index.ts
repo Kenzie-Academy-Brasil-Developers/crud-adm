d314ddfc84ed8c8e20e861ad854b84983293b491
Vitor Galdino <httpvittor@gmail.com>
2023-02-15 21:43:25 -0300

    feat: included "verifyAdminAccess" middleware on GET route

--- src/routers/users/index.ts
+++ src/routers/users/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { createUsersController, getAllUsersController, usersLoginController } from '../../controllers/users';
-import { checkUserEmail, verifyToken } from '../../middlewares/users';
+import { checkUserEmail, verifyAdminAccess, verifyToken } from '../../middlewares/users';
 import { validateBody } from '../../middlewares/validateBody';
 import { loginSchema, userSchema } from '../../schemas/users';
 
@@ -8,6 +8,6 @@ export const userRoutes: Router = Router();
 export const loginRoute: Router = Router();
 
 userRoutes.post('', validateBody(userSchema), checkUserEmail, createUsersController);
-userRoutes.get('', verifyToken, getAllUsersController);
+userRoutes.get('', verifyToken, verifyAdminAccess, getAllUsersController);
 
 loginRoute.post('', validateBody(loginSchema), usersLoginController);
