a74bfca103ab8238001a05a62aef08fbdb2a0efd
Vitor Galdino <httpvittor@gmail.com>
2023-02-16 00:25:04 -0300

    feat: included userId key to Express

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -3,7 +3,8 @@ import * as express from 'express';
 declare global {
     namespace Express {
         interface Request {
-            userEmail: string | void;
+            userEmail: string;
+            userId: number;
             isAdmin: boolean;
         }
     }
