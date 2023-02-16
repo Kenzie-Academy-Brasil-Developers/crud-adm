a3a966d5a1793f95dddbb2a6c81afaf34abfe000
Vitor Galdino <httpvittor@gmail.com>
2023-02-16 08:55:25 -0300

    feat: creted PUT route to handle with userRoutes

--- src/routers/users/index.ts
+++ src/routers/users/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { createUsersController, getAllUsersController, getLoggedUserController, softDeleteController, usersLoginController, userUpdateController } from '../../controllers/users';
+import { activateUserController, createUsersController, getAllUsersController, getLoggedUserController, softDeleteController, usersLoginController, userUpdateController } from '../../controllers/users';
 import { checkPermission, checkUserEmail, checkUserId, verifyAdminAccess, verifyToken } from '../../middlewares/users';
 import { validateBody } from '../../middlewares/validateBody';
 import { loginSchema, userSchema, userUpdateSchema } from '../../schemas/users';
@@ -13,5 +13,6 @@ userRoutes.get('', verifyToken, verifyAdminAccess, getAllUsersController);
 userRoutes.get('/profile', verifyToken, getLoggedUserController);
 userRoutes.patch('/:id', validateBody(userUpdateSchema), checkUserId, checkUserEmail, verifyToken, verifyAdminAccess, checkPermission, userUpdateController);
 userRoutes.delete('/:id', checkUserId, verifyToken, verifyAdminAccess, checkPermission, softDeleteController);
+userRoutes.put('/:id/recover', checkUserId, verifyToken, verifyAdminAccess, activateUserController);
 
 loginRoute.post('', validateBody(loginSchema), usersLoginController);
