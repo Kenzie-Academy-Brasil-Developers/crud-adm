2866b3ea399d0f5efaa98eeac7bd90859525c581
Vitor Galdino <httpvittor@gmail.com>
2023-02-17 14:33:57 -0300

    fix: change admin key to be optional in "userSchema"

--- src/schemas/users/index.ts
+++ src/schemas/users/index.ts
@@ -4,7 +4,7 @@ export const userSchema = z.object({
     name: z.string().min(3).max(20),
     email: z.string().email().max(100),
     password: z.string().min(3).max(120),
-    admin: z.boolean(),
+    admin: z.boolean().optional(),
 });
 
 export const userUpdateSchema = z.object({
